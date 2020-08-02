12d3c1667ef84f1f27a3ccdcb08a39fa7a50d912
Pavel Yadlouski <46033323+x00Pavel@users.noreply.github.com>
2020-08-02 18:34:22 +0200

    Update and rename a.sh to start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget "https://github.com/x00Pavel/todo_after_install/archive/master.zip"
+# wget "https://github.com/x00Pavel/todo_after_install/archive/master.zip"
 unzip master.zip
 cd todo_after_install-master
 bash todo_after_install_ubuntu.sh
